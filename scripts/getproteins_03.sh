8021b9e0bba9b0fa1b27802032c91ccd5a4253eb
Francisco M Couto <fjcouto@edu.ulisboa.pt>
2026-02-18 14:58:45 +0000

    deleted files

--- scripts/getproteins_03.sh
+++
@@ -1,9 +0,0 @@
-url="https://www.ebi.ac.uk/ebisearch/ws/rest/chebi/entry/$1/xref/UniProtKB?size=100&format=csv"
-
-curl -s "$url" | \
-    grep \
-      -e 'HUMAN' \
-      -e 'RAT' \
-      -e 'MOUSE' | \
-    cut -d, -f5 | \
-    tr -d \"
