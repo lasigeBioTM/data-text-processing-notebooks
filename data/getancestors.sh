062fb4d9cc457e6634e859101a36a884d19b19cf
Francisco M Couto <fjcouto@edu.ulisboa.pt>
2026-08-26 11:35:40 +0100

    Delete data/getancestors.sh

--- data/getancestors.sh
+++
@@ -1,6 +0,0 @@
-OWLFILE=$1
-CLASSES=$(cat -)
-[[ -z "$CLASSES" ]] && exit
-PARENTS=$(echo "$CLASSES" | ./getparents.sh $OWLFILE | sort -u)
-echo "$PARENTS"
-echo "$PARENTS" | ./getancestors.sh $OWLFILE
