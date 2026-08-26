2a0d415d55815223c8f1179cabd6c81dd06a1f54
Francisco M Couto <fjcouto@edu.ulisboa.pt>
2026-08-26 11:36:38 +0100

    Delete data/geturi.sh

--- data/geturi.sh
+++
@@ -1,2 +0,0 @@
-OWLFILE=$1
-xargs -I {} xmllint --xpath "//*[(local-name()='hasExactSynonym' or local-name()='hasRelatedSynonym' or local-name()='label') and text()='{}']/../@*[local-name()='about']" $OWLFILE | cut -d'"' -f2
