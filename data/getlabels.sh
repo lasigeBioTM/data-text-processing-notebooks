b7028fac93ab99411c3e38d849fe9824fbee3891
Francisco M Couto <fjcouto@edu.ulisboa.pt>
2026-08-26 11:35:50 +0100

    Delete data/getlabels.sh

--- data/getlabels.sh
+++
@@ -1,2 +0,0 @@
-OWLFILE=$1
-xargs -I {} xmllint --xpath "//*[local-name()='Class'][@*[local-name()='about']='{}']/*[local-name()='hasExactSynonym' or local-name()='hasRelatedSynonym' or local-name()='label']/text()" $OWLFILE
